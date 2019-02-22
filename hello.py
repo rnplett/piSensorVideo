f5d4817617e37063ba037394bb4e1ebaf52a1e5a
Roland Plett <roland@rnplett.org>
2019-02-22 23:51:47 +0000

    The new requirements for python 3.5

--- hello.py
+++ hello.py
@@ -2,6 +2,11 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
+app.run(
+    host="0.0.0.0",
+    port=5000
+)
+
 @app.route('/')
 def index():
     return 'Index Page'
